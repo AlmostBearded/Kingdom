cb6c6607eb52740183d89ff86ff7c6f1ca1ace8d
Peter Oberrauner <thealmostbearded@gmail.com>
2019-05-30 22:09:27 +0200

    Create Ground Tileset and Test Map

---
+++ world/tilesets/tileset.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Tileset" tilewidth="80" tileheight="80" tilecount="36" columns="6">
+ <image source="tileset.png" width="480" height="480"/>
+ <terraintypes>
+  <terrain name="Ground" tile="9"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,0"/>
+ <tile id="3" terrain=",,0,0"/>
+ <tile id="4" terrain=",,0,0"/>
+ <tile id="5" terrain=",,0,"/>
+ <tile id="6" terrain=",0,,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,,0,"/>
+ <tile id="12" terrain=",0,,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,"/>
+ <tile id="15" terrain="0,0,,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,,0,"/>
+ <tile id="18" terrain=",0,,0"/>
+ <tile id="19" terrain="0,0,0,0"/>
+ <tile id="20" terrain="0,,0,0"/>
+ <tile id="21" terrain=",0,0,0"/>
+ <tile id="22" terrain="0,0,0,0"/>
+ <tile id="23" terrain="0,,0,"/>
+ <tile id="24" terrain=",0,,0"/>
+ <tile id="25" terrain="0,0,0,0"/>
+ <tile id="26" terrain="0,0,0,0"/>
+ <tile id="27" terrain="0,0,0,0"/>
+ <tile id="28" terrain="0,0,0,0"/>
+ <tile id="29" terrain="0,,0,"/>
+ <tile id="30" terrain=",0,,"/>
+ <tile id="31" terrain="0,0,,"/>
+ <tile id="32" terrain="0,0,,"/>
+ <tile id="33" terrain="0,0,,"/>
+ <tile id="34" terrain="0,0,,"/>
+ <tile id="35" terrain="0,,,"/>
+</tileset>
